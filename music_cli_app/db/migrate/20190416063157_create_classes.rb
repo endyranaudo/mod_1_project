b5918df69725d2b5bf9eeec3ba0dd4b90851fc58
endyranaudo <endyranaudo@gmail.com>
2019-04-16 07:35:49 +0100

    tyding-up project

--- music_cli_app/db/migrate/20190416063157_create_classes.rb
+++ music_cli_app/db/migrate/20190416063157_create_classes.rb
@@ -1,38 +1,38 @@
 class CreateTables < ActiveRecord::Migration[5.2]
-  # def change
-  #   create_table :users do |t|
-  #     t.string :name
-  #
-  #     t.integer :points, :default => 0
-  #   end
-  # end
-  #
-  #
-  # def change
-  #   create_table :artist do |t|
-  #     t.string :name
-  #
-  #     t.timestamps
-  #   end
-  # end
-  #
-  # def change
-  #   create_table :album do |t|
-  #     t.string :title
-  #     t.string :artist_id
-  #
-  #     t.timestamps
-  #   end
-  # end
-  #
-  #
-  # def change
-  #   create_table :question do |t|
-  #     t.integer :user_id
-  #     t.integer :question_id
-  #
-  #     t.timestamps
-  #   end
-  # end
+  def change
+    create_table :users do |t|
+      t.string :name
+
+      t.integer :points, :default => 0
+    end
+  end
+
+
+  def change
+    create_table :artist do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def change
+    create_table :album do |t|
+      t.string :title
+      t.string :artist_id
+
+      t.timestamps
+    end
+  end
+
+
+  def change
+    create_table :question do |t|
+      t.integer :user_id
+      t.integer :question_id
+
+      t.timestamps
+    end
+  end
 
 end
