fab278c62b6377efd32fe32e0568c8583e64450e
endyranaudo <endyranaudo@gmail.com>
2019-04-16 07:12:36 +0100

    endy's branch created - classes created

--- music_cli_app/app/models/artist.rb
+++ music_cli_app/app/models/artist.rb
@@ -1,3 +1,9 @@
 class Artist < ActiveRecord::Base
+  has_many :albums
+  validates :name, presence: true
+
+  def self.random
+    self.all.sample
+  end
+
 end
-    
--- music_cli_app/app/models/question.rb
+++ music_cli_app/app/models/question.rb
@@ -1,3 +1,14 @@
 class Question < ActiveRecord::Base
+  belongs_to :user
+
+  validates :user_id, presence: true
+
+  # define method to generate question
+  def question_generator
+    album = Album.random
+    right_answer = album.artist.name
+    puts "Who wrote: #{album.name}"
+    user_answer = gets.chomp
+    user_answer == right_answer
+  end
 end
-    
